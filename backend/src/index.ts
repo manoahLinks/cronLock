87b6b38fc6e94ce7d756de7ab74ccf643f422920
manoahLinks <manoahluka@gmail.com>
2025-12-27 19:07:32 +0100

    chore: add logger middleware

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,11 +2,13 @@ import cors from 'cors';
 import express from 'express';
 import * as routes from './routes/index.js';
 import { connectDB } from './mongoose.js';
+import { logger } from './lib/middleware/logger.js';
 
 const app = express();
 const port = process.env.PORT || 8787;
 
 app.use(cors());
+app.use(logger);
 
 routes.register(app);
 
