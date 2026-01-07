e9e2f2c9d09ba436c6514b6518bd97a3b841e947
manoahLinks <manoahluka@gmail.com>
2026-01-07 20:11:43 +0100

    chore: add feilds t device model

--- backend/src/models/device.model.ts
+++ backend/src/models/device.model.ts
@@ -3,11 +3,15 @@ import mongoose, { Schema, Document } from 'mongoose';
 export interface IDevice extends Document {
   deviceId: string;
   description: string;
-}
+  address: string;
+  price: number
+;}
 
 const DeviceSchema = new Schema<IDevice>({
   deviceId: { type: String, required: true, unique: true },
   description: { type: String, required: true },
+  address: {type: String},
+  price: {type: Number}
 });
 
 export const Device = mongoose.model<IDevice>('Device', DeviceSchema);
